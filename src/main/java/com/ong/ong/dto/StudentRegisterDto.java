a4ed7a4092a7ecc21b0148ac18bee0fc7170f0ea
ViniCarmo <vinicius.carmo1106@gmail.com>
2025-05-19 17:03:16 -0300

    create Dto student, teacher

---
+++ src/main/java/com/ong/ong/dto/StudentRegisterDto.java
@@ -0,0 +1,21 @@
+package com.ong.ong.dto;
+
+import java.time.LocalDate;
+
+public record StudentRegisterDto(
+        String nome,
+        String cpf,
+        String email,
+        String senha,
+        String rg,
+        LocalDate dataNascimento,
+        String telefone,
+        String cep,
+        String endereco,
+        String numero,
+        String complemento,
+        String cidade,
+        String estado,
+        String nomeMae,
+        String nomeEscolaAtual
+) {}
---
+++ src/main/java/com/ong/ong/dto/TeacherRegisterDto.java
@@ -0,0 +1,23 @@
+package com.ong.ong.dto;
+
+import java.time.LocalDate;
+
+public record TeacherRegisterDto(
+        String nome,
+        String cpf,
+        String email,
+        String senha,
+        String rg,
+        LocalDate dataNascimento,
+        String telefone,
+        String cep,
+        String endereco,
+        String numero,
+        String complemento,
+        String cidade,
+        String estado,
+        String estadoCivil,
+        String nivelEnsino,
+        String materiaEspecializada,
+        String modalidadeEnsino
+) {}
